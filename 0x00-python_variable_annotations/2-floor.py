1ca514af5026371ee94ece319453d7016bd67436
root <mykelaloh@gmail.com>
2023-10-05 13:39:08 -0700

    task3

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+"""modue containing type annonated floor function"""
+
+import math
+
+
+def floor(n: float) -> int:
+    """annonated function that return floor of arg"""
+    return math.floor(n)
