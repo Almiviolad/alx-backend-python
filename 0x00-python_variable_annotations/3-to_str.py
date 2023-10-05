403c66afadee2457f75fb23214d97b4f45482cbe
root <mykelaloh@gmail.com>
2023-10-05 13:43:58 -0700

    task 3

---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+"""module containing type annonated function thay returns string of arg"""
+
+
+def to_str(n: float) -> str:
+    """type annonated function that retuns string of float"""
+    return str(n)
