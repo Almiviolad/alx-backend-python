517c794e35bc1aacd4d8be1017753ab452a7e41d
root <mykelaloh@gmail.com>
2023-10-05 13:33:42 -0700

    task1

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+"""module containing type annonated function to return concatenated str"""
+
+
+def concat(str1: str, str2: str) -> str:
+    """type annotated function that concatenate 2 strings"""
+    return (str1 + str2)
